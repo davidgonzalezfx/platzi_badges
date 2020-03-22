7c4460aa5353b375b97b09d8e6d9dc3bb7daf7b9
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-22 14:48:01 -0500

    [Feature #2] Styles added

--- platzi-badges/src/components/Badge.js
+++ platzi-badges/src/components/Badge.js
@@ -1,16 +1,21 @@
 import React from "react";
 import logoConf from "../images/badge-header.svg";
+import "./styles/Badge.css";
 
 class Badge extends React.Component {
   render() {
     return (
-      <div>
-        <div>
+      <div className="Badge">
+        <div className="Badge__header">
           <img src={logoConf} alt="Logo de la conferencia" />
         </div>
 
-        <div>
-          <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
+        <div className="Badge__section-name">
+          <img
+            className="Badge__avatar"
+            src="https://pbs.twimg.com/profile_images/1184193988041105409/pr-9lmm7_400x400.jpg"
+            alt="Avatar"
+          />
           <h1>
             David
             <br />
@@ -18,12 +23,12 @@ class Badge extends React.Component {
           </h1>
         </div>
 
-        <div>
-          <p>Fullstack Software Engineer</p>
-          <p>@davidgonzalezfx</p>
+        <div className="Badge__section-info">
+          <h3>Fullstack Software Engineer</h3>
+          <div>@davidgonzalezfx</div>
         </div>
 
-        <div>#PlatziConf</div>
+        <div className="Badge__footer">#PlatziConf</div>
       </div>
     );
   }
--- platzi-badges/src/index.js
+++ platzi-badges/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Badge from "./components/Badge.js";
+import "bootstrap/dist/css/bootstrap.css";
+import "./global.css";
 
 const container = document.getElementById("app");
 
