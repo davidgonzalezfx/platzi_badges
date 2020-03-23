dcb4821d8e204e5f0b353f1082cf9355164d3365
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-22 22:24:46 -0500

    [Feature #5] List badges page: complete list

--- platzi-badges/src/index.js
+++ platzi-badges/src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import BadgeNew from "./pages/BadgeNew.js";
+import Badges from "./pages/Badges.js";
 import "bootstrap/dist/css/bootstrap.css";
 import "./global.css";
 
 const container = document.getElementById("app");
 
-ReactDOM.render(<BadgeNew />, container);
+ReactDOM.render(<Badges />, container);
