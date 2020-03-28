b1b8bdc7cc0b7861dbfc58156c0b6fea01009d4b
davidgonzalezfx <dvdramos16@gmail.com>
2020-03-28 17:08:46 -0500

    [Test] Trying hooks with presentational component

--- platzi-badges/src/pages/BadgeDetails.js
+++ platzi-badges/src/pages/BadgeDetails.js
@@ -6,65 +6,82 @@ import Logo from '../images/platziconf-logo.svg';
 import Badge from '../components/Badge';
 import DeleteBadgeModal from '../components/DeleteBadgeModal';
 
-export default class BadgeDetails extends Component {
-  render() {
-    return (
-      <div>
-        <div className='BadgeDetails__hero'>
-          <div className='container'>
-            <div className='row'>
-              <div className='col-6'>
-                <img src={Logo} alt='Logo Platzi Conf' />
-              </div>
-              <div className='col-6 BadgeDetails__hero-attentandt-name'>
-                <h1>
-                  {this.props.badge.firstName} {this.props.badge.lastName}
-                </h1>
-              </div>
+function useIncreaseCount(max) {
+  const [count, setCount] = React.useState(0);
+
+  if (count > max) {
+    setCount(0);
+  }
+
+  return [count, setCount];
+}
+
+function BadgeDetails(props) {
+  const [count, setCount] = useIncreaseCount(4);
+
+  return (
+    <div>
+      <div className='BadgeDetails__hero'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-6'>
+              <img src={Logo} alt='Logo Platzi Conf' />
+            </div>
+            <div className='col-6 BadgeDetails__hero-attentandt-name'>
+              <h1>
+                {props.badge.firstName} {props.badge.lastName}
+              </h1>
             </div>
           </div>
         </div>
+      </div>
 
-        <div className='container'>
-          <div className='row'>
-            <div className='col'>
-              <Badge
-                firstName={this.props.badge.firstName}
-                lastName={this.props.badge.lastName}
-                email={this.props.badge.email}
-                jobTitle={this.props.badge.jobTitle}
-                twitter={this.props.badge.twitter}
-              />
-            </div>
-            <div className='col'>
-              <h2>Actions</h2>
+      <div className='container'>
+        <div className='row'>
+          <div className='col'>
+            <Badge
+              firstName={props.badge.firstName}
+              lastName={props.badge.lastName}
+              email={props.badge.email}
+              jobTitle={props.badge.jobTitle}
+              twitter={props.badge.twitter}
+            />
+          </div>
+          <div className='col'>
+            <h2>Actions</h2>
+            <div>
               <div>
-                <div>
-                  <Link
-                    className='btn btn-primary mb-4'
-                    to={`/badges/${this.props.badge.id}/edit`}
-                  >
-                    Edit
-                  </Link>
-                </div>
-                <div>
-                  <button
-                    onClick={this.props.onOpenModal}
-                    className='btn btn-danger'
-                  >
-                    Delete
-                  </button>
-                  <DeleteBadgeModal
-                    isOpen={this.props.modalIsOpen}
-                    onClose={this.props.onCloseModal}
-                    onDeleteBadge={this.props.onDeleteBadge}
-                  />
-                </div>
+                <button
+                  onClick={() => {
+                    setCount(count + 1);
+                  }}
+                  className='btn btn-primary mr-4'
+                >
+                  Increase count {count}
+                </button>
+                <Link
+                  className='btn btn-primary mb-4'
+                  to={`/badges/${props.badge.id}/edit`}
+                >
+                  Edit
+                </Link>
+              </div>
+              <div>
+                <button onClick={props.onOpenModal} className='btn btn-danger'>
+                  Delete
+                </button>
+                <DeleteBadgeModal
+                  isOpen={props.modalIsOpen}
+                  onClose={props.onCloseModal}
+                  onDeleteBadge={props.onDeleteBadge}
+                />
               </div>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default BadgeDetails;
